d397b631c7a295f7a2791749d5c64b1de9a2b5bc
oithegod <serejathegod@gmail.com>
2023-03-30 23:34:54 +0300

    edited urls.py

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -7,17 +7,24 @@ app_name = "newspaper"
 
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("redactor/create/", views.RedactorCreateView.as_view(), name="redactor-create"),
+    path("", views.index, name="index"),
     path("topics/", views.TopicListView.as_view(), name="topic-list"),
     path("topics/create/", views.TopicCreateView.as_view(), name="topic-create"),
     path("topics/<int:pk>/update/", views.TopicUpdateView.as_view(), name="topic-update"),
     path("topics/<int:pk>/delete/", views.TopicDeleteView.as_view(), name="topic-delete"),
     path("topics/<int:pk>/", views.TopicDetailView.as_view(), name="topic-detail"),
     path("newspapers/create/", views.NewspaperCreateView.as_view(), name="newspaper-create"),
+    path("newspapers/<int:pk>/", views.NewspaperDetailView.as_view(), name="newspaper-detail"),
     path("newspapers/<int:pk>/update/", views.NewspaperUpdateView.as_view(), name="newspaper-update"),
     path("newspapers/<int:pk>/delete/", views.NewspaperDeleteView.as_view(), name="newspaper-delete"),
     path("newspapers/", views.NewspaperListView.as_view(), name="newspaper-list"),
+    path("redactors/<int:pk>/", views.RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactors/list/", views.RedactorListView.as_view(), name="redactor-list"),
+    path("redactors/<int:pk>/delete/", views.RedactorDeleteView.as_view(), name="redactor-delete"),
+    path("redactors/<int:pk>/update/", views.RedactorUpdateView.as_view(), name="redactor-update"),
+    path("sign-in/", views.LoginView.as_view(), name="sign-in"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("sign-up/", views.RedactorCreateView.as_view(), name="sign-up"),
 
 ]
 
