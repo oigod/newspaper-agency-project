d69fa65d4eb91869f6a8f3580cae68c6434971e5
oithegod <serejathegod@gmail.com>
2023-03-31 00:14:58 +0300

    edited project settings added context_proccesors.py for future custom template

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'newspaper_agency.context_processors.cfg_assets_root',
             ],
         },
     },
@@ -128,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 
-
+ASSETS_ROOT = "/static/assets"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newspaper.urls', namespace='newspaper')),
-    path("accounts/", include("django.contrib.auth.urls")),
 
 ]
