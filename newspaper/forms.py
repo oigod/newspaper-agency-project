a93125ed5da23cdd6588ecf404d67a126919d492
oithegod <serejathegod@gmail.com>
2023-03-30 23:17:40 +0300

    Edited form.py file implement search features

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
 
 from newspaper.models import Redactor, Newspaper
 
@@ -15,6 +15,9 @@ class RedactorCreationForm(UserCreationForm):
             "years_of_experience",
         )
 
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
 
 class NewspaperForm(forms.ModelForm):
     publishers = forms.ModelMultipleChoiceField(
@@ -25,3 +28,39 @@ class NewspaperForm(forms.ModelForm):
     class Meta:
         model = Newspaper
         fields = "__all__"
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        required=False,
+        max_length=255,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Title"}),
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        required=False,
+        max_length=255,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Name"}),
+    )
+
+
+class RedactorUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Redactor
+        fields = ["first_name", "last_name", "years_of_experience"]
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+def validate_years_of_experience(years_of_experience):
+    if years_of_experience < 0:
+        raise ValidationError("Years of experience should be positive")
+    elif years_of_experience > 100:
+        raise ValidationError("Years of experience should be less than 100")
+
+    return years_of_experience
