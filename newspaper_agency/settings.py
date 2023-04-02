dc750f750753073a3a9398cad5982f802300c28f
oithegod <serejathegod@gmail.com>
2023-04-02 21:44:57 +0300

    Removed insecure Django secret key from settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8u^zby*36!pzc)syllt+rbv6brss488gj)#63ak$r_u$n1gunw')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
