21d6d3154043b516b5ebcb8ca988d2e6f55f96b3
oithegod <serejathegod@gmail.com>
2023-04-02 20:16:57 +0300

    Updated settings & requirements.txt

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8u^zby*36!pzc)syllt+rbv6brss488gj)#63ak$r_u$n1gunw'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8u^zby*36!pzc)syllt+rbv6brss488gj)#63ak$r_u$n1gunw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -142,3 +142,4 @@ LOGIN_REDIRECT_URL = "/"
 MEDIA_URL = "media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
+
