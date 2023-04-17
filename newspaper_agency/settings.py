dc5e9b6c598bc63b8959b35059afa392b6be9532
oithegod <serejathegod@gmail.com>
2023-04-17 13:43:06 +0300

    Edited settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -16,7 +16,8 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR/".env")
+if os.path.exists(BASE_DIR/".env"):
+    load_dotenv(BASE_DIR/".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
