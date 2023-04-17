8c4777d0da6b6143a088307bd3ad1770fb5ffb9a
oithegod <serejathegod@gmail.com>
2023-04-17 14:46:35 +0300

    Trying to fix problem with image

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-z8y6.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
