e24d123d3285349d7657cff2594545eb043e264d
oithegod <serejathegod@gmail.com>
2023-04-17 13:40:55 +0300

    Added new configuration for models, Created migrations, added test file for test

--- newspaper/models.py
+++ newspaper/models.py
@@ -6,9 +6,9 @@ class Topic(models.Model):
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     short_description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    image = models.ImageField(upload_to="images/%Y/%m/%d/", blank=True, null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return a string representation of the model."""
         return self.name
 
@@ -23,7 +23,7 @@ class Redactor(AbstractUser):
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return a string representation of the model."""
         return f"{self.first_name} {self.last_name} {self.years_of_experience}"
 
@@ -34,12 +34,12 @@ class Newspaper(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="newspapers")
     publishers = models.ManyToManyField(Redactor, related_name="newspapers")
-    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    image = models.ImageField(upload_to="images/%Y/%m/%d/", blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "newspapers"
         verbose_name = "newspaper"
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return a string representation of the model."""
         return self.title
